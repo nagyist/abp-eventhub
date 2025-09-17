0e9fde4baceca642f9876a140452463ef368ff83
Yunus Emre Kalkan <emre.kalkan@volosoft.com>
2025-09-17 13:58:45 +0300

    Added migration

--- src/EventHub.EntityFrameworkCore/Migrations/20250917105808_Upgrade_Abp_Version_9_3_3.Designer.cs
+++ src/EventHub.EntityFrameworkCore/Migrations/20250917105808_Upgrade_Abp_Version_9_3_3.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EventHub.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Volo.Abp.EntityFrameworkCore;
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace EventHub.Migrations
 {
     [DbContext(typeof(EventHubDbContext))]
-    partial class EventHubDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250917105808_Upgrade_Abp_Version_9_3_3")]
+    partial class Upgrade_Abp_Version_9_3_3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
