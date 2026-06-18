622f4523e401de288ae047f7fa19c8304e489a20
Engincan VESKE <enginveske@gmail.com>
2026-06-18 17:39:37 +0300

    Add ABP 10.4 EF migration

--- src/EventHub.EntityFrameworkCore/Migrations/20260618143857_Upgrade_Abp_Version_10_4.Designer.cs
+++ src/EventHub.EntityFrameworkCore/Migrations/20260618143857_Upgrade_Abp_Version_10_4.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EventHub.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Volo.Abp.EntityFrameworkCore;
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace EventHub.Migrations
 {
     [DbContext(typeof(EventHubDbContext))]
-    partial class EventHubDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260618143857_Upgrade_Abp_Version_10_4")]
+    partial class Upgrade_Abp_Version_10_4
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
